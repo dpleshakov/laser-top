d1626bd753950d6c9acf5e3abfc16ad46c504623
Dmitry Pleshakov <dpleshakov@gmail.com>
2013-04-17 16:30:44 +0400

    Обновил логику расчет ачивок.

--- achievements.py
+++ achievements.py
@@ -1,8 +1,24 @@
+import webapp2
 from google.appengine.ext import db
+import jinja2
+import os
+import datetime, time
 
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+###################################################
 class Command(db.Model):
 	name = db.StringProperty(required = True)
 	# gamers
+
+	@property
+	def keyStr(self):
+		return str(self.key())
+
+	@property
+	def gamersCount(self):
+		return self.gamers.count()
 
 class Gamer(db.Model):
 	name = db.StringProperty(required = True)
@@ -10,6 +26,27 @@ class Gamer(db.Model):
 	command = db.ReferenceProperty(Command, collection_name = 'gamers')
 	# stats
 	# achievements
+
+	@property
+	def keyStr(self):
+		return str(self.key())
+
+	@property
+	def gamesCount(self):
+		return self.stats.count()
+
+class Game(db.Model):
+	date = db.DateProperty(required = True)
+	# stats
+	# achievements
+
+	@property
+	def keyStr(self):
+		return str(self.key())
+
+	@property:
+	def gamersCount(self):
+		return self.stats.count()
 
 class Statistic(db.Model):
 	game = db.ReferenceProperty(Game, collection_name = 'stats', required = True)
@@ -22,21 +59,18 @@ class Statistic(db.Model):
 	countOfInjuries = db.IntegerProperty(required = True)
 	usedCartridge = db.IntegerProperty(required = True)
 
-class Achievement(db.Model):
-	achievementType = db.ReferenceProperty(AchievementType, collection_name = 'achievements', required = True)
-	game = db.ReferenceProperty(Game, collection_name = 'achievements', required = True)
-	gamer = db.ReferenceProperty(Gamer, collection_name = 'achievements', required = True)
-
 class AchievementType(db.Model):
 	name = db.StringProperty(required = True)
 	level = db.StringProperty(required = True)
 	image = db.LinkProperty(default = None)
 	# achievements
+	
+class Achievement(db.Model):
+	achievementType = db.ReferenceProperty(AchievementType, collection_name = 'achievements', required = True)
+	game = db.ReferenceProperty(Game, collection_name = 'achievements', required = True)
+	gamer = db.ReferenceProperty(Gamer, collection_name = 'achievements', required = True)
 
-class Game(db.Model):
-	date = db.DateProperty(required = True)
-	# stats
-	# achievements
+###################################################
 
 def GetAchievementType(name, level):
 	return AchievementType.all().filter('name = ', name).filter('level = ', level).fetch(1)
