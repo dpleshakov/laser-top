8fd41799a422fce6602ca81fcbec8f9d29bece99
Dmitry Pleshakov <dpleshakov@gmail.com>
2013-05-08 21:22:48 +0400

    Исправил ошибки.

--- main.py
+++ main.py
@@ -31,6 +31,12 @@ JINJA_ENVIRONMENT.filters['DateFormat'] = DateFormat
 JINJA_ENVIRONMENT.filters['GamerFormat'] = GamerFormat
 
 ######################################################################################################
+class News(db.Model):
+	date = db.DateProperty(required = True, default = datetime.datetime.now().date())
+	title = db.StringProperty(required = True)
+	body = db.Text()
+
+###################################################
 class Image(db.Model):
 	data = db.BlobProperty(default = None)
 	# commands
@@ -104,130 +110,20 @@ class Achievement(db.Model):
 	gamer = db.ReferenceProperty(Gamer, collection_name = 'achievements', required = True)
 
 ######################################################################################################
-@db.transactional
-@loggingWrapper.CallFunction
-def AddGamer(name, nick):
-	gamer = Gamer(name = name, nick = nick)
-	gamer.put()
-	logging.info("Add new Gamer('" + name + "', '" + nick + "').")
-	return gamer
-
-###################################################
-@db.transactional
-@loggingWrapper.CallFunction
-def AddGame(date):
-	game = Game(
-		date = date,
-		wasCalculated = False)
-	game.put()
-	logging.info("Add new Game('" + str(date) + "').")
-	return game
-
-###################################################
-@loggingWrapper.CallFunction
-def GetGamer(name, nick):
-	logging.info("Try get gamer by name.")
-	gamer = Gamer.gql("WHERE name = :1", name)
-	if gamer.count() > 0:
-		logging.info("Find gamer.")
-		return gamer[0]
-	
-	logging.info("Try get gamer by nick.")
-	gamer = Gamer.gql("WHERE nick = :1", nick)
-	if gamer.count() > 1 and not nick == 'None':
-		logging.info("Find gamer.")
-		return gamer[0]
-
-	logging.info("Try add new gamer.")
-	return AddGamer(name, nick)
-
-###################################################
-@loggingWrapper.CallFunction
-def GetGame(date):
-	logging.info("Try get game by date.")
-	game = Game.gql("WHERE date = :1", date)
-	if game.count() > 0:
-		logging.info("Find game.")
-		return game[0]
-
-	logging.info("Try add new game.")
-	return AddGame(date)
-
-###################################################
-@loggingWrapper.CallFunction
-def StrToDate(dateAsStr):
-	dateAsTime = time.strptime(dateAsStr, "%d.%m.%Y")
-	date = datetime.date(dateAsTime.tm_year, dateAsTime.tm_mon, dateAsTime.tm_mday)
-	logging.info("Convert '" + str(dateAsTime) + "' to '" + str(date) + "'.")
-	return date
-
-###################################################
-@loggingWrapper.CallFunction
-def ParseLine(line):
-	tabulationChar = '\t'
-	logging.info("Line to parse '" + line + "'.")
-	splitedLine = line.split(tabulationChar)
-
-	splitedLine[0] = StrToDate(splitedLine[0])
-	splitedLine[4] = float(splitedLine[4])
-	splitedLine[5] = float(splitedLine[5])
-	splitedLine[6] = int(splitedLine[6])
-	splitedLine[7] = int(splitedLine[7])
-	splitedLine[8] = int(splitedLine[8])
-	splitedLine[9] = int(splitedLine[9])
-
-	return splitedLine
-
-###################################################
-@loggingWrapper.CallFunction
-def AddStats(stats):
-	for stat in stats:
-		game = GetGame(stat[0])
-		gamer = GetGamer(stat[2], stat[3])
-
-		# existingStats = Statistic.all().filter('game =', game).filter('gamer =', gamer)
-		existingStats = Statistic.gql("WHERE game = :1 AND gamer = :2", game, gamer)
-		if existingStats.count() == 0:
-			Statistic(
-				game = GetGame(stat[0]),
-				color = stat[1],
-				gamer = GetGamer(stat[2], stat[3]),
-				rating = stat[4],
-				accuracy = stat[5],
-				damage = stat[6],
-				countOfDeaths = stat[7],
-				countOfInjuries = stat[8],
-				usedCartridge = stat[9],
-			).put()
-			logging.info("Add new Statistic.")
-		else:
-			logging.info("There is Statistic for thie game and gamer.")
-
-###################################################
-@loggingWrapper.CallFunction
-def Parse(text):
-	newLineChar = '\n'
-	text = text.replace('%', '')
-	text = text.replace(',', '.')
-	lines = text.split(newLineChar)
-	stats = []
-	for line in lines:
-		stats.append(ParseLine(line))
-	AddStats(stats)
-
-######################################################################################################
 class MainPage(webapp2.RequestHandler):
 	def get(self):
 		gamers = Gamer.all().order('name')
 		commands = Command.all().order('name')
 		statistics = Statistic.all().order('-rating')
 		games = Game.all().order('date')
+		news = News.all().order('date')
 
 		template_values = {
 			'gamers': gamers,
 			'commands': commands,
 			'games': games,
 			'statistics': statistics,
+			'news': news,
 		}
 
 		template = JINJA_ENVIRONMENT.get_template('index.html')
